b8c6736c8711bb6d54c2cfd677f8db7635195906
alvinirwanto <irwantoalvin@gmail.com>
2023-06-14 10:08:08 +0700

    add google tag script (update)

--- pages/_document.js
+++ pages/_document.js
@@ -1,21 +1,22 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
     return (
         <Html lang='en'>
             <Head>
                 {/* <!-- Google tag (gtag.js) -->  */}
-                <script async src="https://www.googletagmanager.com/gtag/js?id=AW-949610150"></script>
-                <script>
-                    window.dataLayer = window.dataLayer || [];
+                <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-949610150"></Script>
+                <Script>
+                    window && window.dataLayer = window.dataLayer || [];
 
                     function gtag(){
                         dataLayer.push(arguments)
                     }
-                    
-                    gtag('js', new Date()); 
+
+                    gtag('js', new Date());
                     gtag('config', 'AW-949610150');
-                </script>
+                </Script>
             </Head>
             <body>
                 <Main />
